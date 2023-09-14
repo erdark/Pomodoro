7cb44071260fc8cae57c85613683af456779b62b
riguet222 <riguet222@unicaen.fr>
2023-09-14 21:17:38 +0200

    ajout du chronomètre

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let chrono = document.getElementById("chrono");
-chrono.addEventListener("click", () =>{
-    let timer = document.getElementById("timer");
-    timer.textContent = "24";
-});
+const minutes = document.getElementById("minutes");
+const secondes = document.getElementById("secondes");
 
+function affichSecondes (secondes) {
+    let secondeschr = secondes.toString()
+    secondeschr = secondeschr.length < 2 ? '0' + secondeschr : secondeschr
+    return secondeschr
+}
+function ecoule_temps(){
+    if(secondes.textContent == 0){
+        minutes.textContent = minutes.textContent - 1;
+        secondes.textContent = 60;
+    }
+    secondes.textContent = secondes.textContent - 1;
+    secondes.textContent = affichSecondes(secondes.textContent)
+}
+
+function depart(){
+    setInterval(ecoule_temps, 1000);
+}
+
+chrono.addEventListener("click", depart);
