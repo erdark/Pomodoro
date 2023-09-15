c1f0d30e0b26062934336ba884553443779f227f
riguet222 <riguet222@unicaen.fr>
2023-09-15 11:58:27 +0200

    correction du chronomètre

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const minutes = document.getElementById("minutes");
 const secondes = document.getElementById("secondes");
+const start = document.getElementById("start");
+const etat = document.getElementById("etat");
 
 function affichSecondes (secondes) {
     let secondeschr = secondes.toString()
     secondeschr = secondeschr.length < 2 ? '0' + secondeschr : secondeschr
     return secondeschr
 }
-function ecoule_temps(){
-    if(secondes.textContent == 0){
+function ecoule_temps_travail(){
+    if(secondes.textContent == 0 && minutes.textContent != 0){
         minutes.textContent = minutes.textContent - 1;
         secondes.textContent = 60;
     }
-    secondes.textContent = secondes.textContent - 1;
+    if(secondes.textContent > 0){
+        secondes.textContent = secondes.textContent - 1;
+    }
     secondes.textContent = affichSecondes(secondes.textContent)
+    minutes.textContent = affichSecondes(minutes.textContent)
 }
 
 function depart(){
-    setInterval(ecoule_temps, 1000);
+    start.textContent = "reset";
+    if(etat.textContent == "work"){
+        setInterval(ecoule_temps_travail, 1000);
+    }
+    else if(etat.textContent == "pause"){
+        setInterval(ecoule_temps_pause, 1000);
+    }
 }
 
-chrono.addEventListener("click", depart);
+start.addEventListener("click", depart);
