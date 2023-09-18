083609c4fbc2a33a95c2879acef74d1f9c744e4e
riguet222 <riguet222@unicaen.fr>
2023-09-18 15:46:46 +0200

    v2 terminé

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const secondes = document.getElementById("secondes");
 const start = document.getElementById("start");
 const etat = document.getElementById("etat");
 
-const Temps_Travail = document.getElementById("Temps_Travail")
+const Temps_Travail = document.getElementById("Temps_Travail");
+const Temps_Pause = document.getElementById("Temps_Pause");
 
+let sauvegarde_travail = 25;
+let sauvegarde_pause = 25;
 let arret_interval = undefined; // Utilisez let au lieu de const pour pouvoir réassigner la variable
 
 function affichSecondes(secondes) {
@@ -27,12 +30,12 @@ function ecoule_temps() {
         secondes.textContent = secondes.textContent - 1;
     }
     if(minutes.textContent == 0 && secondes.textContent == 0 && etat.textContent == "travail"){
-        minutes.textContent = 5;
+        minutes.textContent = sauvegarde_pause;
         document.body.style.backgroundColor = "darkblue";
         etat.textContent = "pause"
     }
     else if(minutes.textContent == 0 && secondes.textContent == 0 && etat.textContent == "pause"){
-        minutes.textContent = 25;
+        minutes.textContent = affichSecondes(sauvegarde_travail);
         document.body.style.backgroundColor = "black"
         etat.textContent = "travail"
     }
@@ -45,15 +48,28 @@ function depart() {
     if (start.textContent == "start") {
         start.textContent = "reset";
         clearInterval(arret_interval); // Efface l'intervalle précédent
-        arret_interval = setInterval(ecoule_temps, 1000);
+        arret_interval = setInterval(ecoule_temps, 10);
     }
     else if (start.textContent == "reset") {
         start.textContent = "start";
         etat.textContent = "travail"
         document.body.style.backgroundColor = "black"
-        minutes.textContent = "25";
+        minutes.textContent = sauvegarde_travail;
         secondes.textContent = "00";
     }
 }
 
+function modifier() {
+    if(Temps_Travail.value >= 1 && Temps_Travail.value <= 60){
+        console.log(Temps_Travail.value)
+        minutes.textContent = affichSecondes(parseInt(Temps_Travail.value));
+        sauvegarde_travail = minutes.textContent;
+    }
+    if(Temps_Pause.value >= 1 && Temps_Pause.value <= 60){
+        console.log(Temps_Pause.value)
+        sauvegarde_pause = affichSecondes(parseInt(Temps_Pause.value));
+    }
+}
+
 start.addEventListener("click", depart);
+soumettre.addEventListener("click", modifier);
